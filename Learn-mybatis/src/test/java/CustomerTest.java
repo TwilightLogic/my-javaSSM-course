69195903cebd248bda6c83ca56e2f53ea0ec4c0d
TwilightLogic <twilightlogic@gmail.com>
2023-03-06 11:49:37 +0800

    delete sqlSession.close() in Learn-mybatis project

--- Learn-mybatis/src/test/java/CustomerTest.java
+++ Learn-mybatis/src/test/java/CustomerTest.java
@@ -22,8 +22,6 @@ public class CustomerTest {
             for (Customer customer: customers) {
                 System.out.println(customer);
             }
-
-            sqlSession.close();
         }
     }
 }
