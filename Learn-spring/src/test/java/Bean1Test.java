00f7d89775be58eddef9c97f59a9fbe6f944a844
TwilightLogic <twilightlogic@gmail.com>
2023-04-03 10:48:35 +0800

    Learning instantiation of bean by using Cconstructor

--- Learn-spring/src/test/java/Bean1Test.java
+++ Learn-spring/src/test/java/Bean1Test.java
@@ -7,6 +7,7 @@ public class Bean1Test {
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        // 其实这个section学的是bean的实例化
         Bean1 bean1 = (Bean1) applicationContext.getBean("bean1");
 
         System.out.println(bean1);
