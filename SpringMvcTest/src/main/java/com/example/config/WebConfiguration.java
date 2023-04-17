1255b888b8987cb72db737e69d084de70bd041dc
TwilightLogic <twilightlogic@gmail.com>
2023-04-17 13:45:50 +0800

    Success to configure the spring and tomcat (FINALLY!!!!)

--- SpringMvcTest/src/main/java/com/example/config/WebConfiguration.java
+++ SpringMvcTest/src/main/java/com/example/config/WebConfiguration.java
@@ -1,7 +1,43 @@
 package com.example.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
+@ComponentScan("com.example.controller")
 public class WebConfiguration {
+
+
+    //我们需要使用ThymeleafViewResolver作为视图解析器，并解析我们的HTML页面
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(@Autowired SpringTemplateEngine springTemplateEngine){
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+        resolver.setOrder(1);   //可以存在多个视图解析器，并且可以为他们设定解析顺序
+        resolver.setCharacterEncoding("UTF-8");   //编码格式是重中之重
+        resolver.setTemplateEngine(springTemplateEngine);   //和之前JavaWeb阶段一样，需要使用模板引擎进行解析，所以这里也需要设定一下模板引擎
+        return resolver;
+    }
+
+    //配置模板解析器
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setSuffix(".html");   //需要解析的后缀名称
+        resolver.setPrefix("/WEB-INF/template/");   //需要解析的HTML页面文件存放的位置
+        return resolver;
+    }
+
+    //配置模板引擎Bean
+    @Bean
+    public SpringTemplateEngine springTemplateEngine(@Autowired ITemplateResolver resolver){
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+        engine.setTemplateResolver(resolver);   //模板解析器，默认即可
+        return engine;
+    }
 }
