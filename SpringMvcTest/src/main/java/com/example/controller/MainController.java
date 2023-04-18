60fa9c8a3c4d01450d9bc16fb491aee6234713b8
TwilightLogic <twilightlogic@gmail.com>
2023-04-18 15:43:43 +0800

    Restrict accessing to the web site with parameters (like username and password)

--- SpringMvcTest/src/main/java/com/example/controller/MainController.java
+++ SpringMvcTest/src/main/java/com/example/controller/MainController.java
@@ -10,15 +10,22 @@ import org.springframework.web.servlet.ModelAndView;
 // 表示添加一个路径前缀
 @RequestMapping("yyds")
 public class MainController {
+    // 我们可以使用`params`属性来指定请求必须携带哪些请求参数
+    // 人话："要携带哪些参数才能访问我们的网页呢？"
+    // 就这样写就好了："http://localhost:8080/mvc/yyds/index?username=xxxx"
+    @RequestMapping(value = "/index", params = "username")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
 
     // 也可以直接写成 @PostMapping("/index") @GetMapping("/index")
     // 如果我们只希望通过GET, POST请求去访问这个路径，怎么写呢？（默认是GET请求）
-    @RequestMapping(value = "/index", method = {RequestMethod.GET, RequestMethod.POST})
-    public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.getModel().put("name", "Damn");
-        return modelAndView;
-    }
+    //    @RequestMapping(value = "/index", method = {RequestMethod.GET, RequestMethod.POST})
+    //    public ModelAndView index() {
+    //        ModelAndView modelAndView = new ModelAndView("index");
+    //        modelAndView.getModel().put("name", "Damn");
+    //        return modelAndView;
+    //    }
 
     // http://localhost:8080/mvc/yyds/index
     //    @RequestMapping("/index")
