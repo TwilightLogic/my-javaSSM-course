b90578f8fd632b51c6da0d658798565953cab1cc
TwilightLogic <twilightlogic@gmail.com>
2023-04-20 16:12:36 +0800

    anoter way to use `method` of RequestMapping

--- SpringMVCAnnotation/src/main/java/com/itheima/controller/MethodController.java
+++ SpringMVCAnnotation/src/main/java/com/itheima/controller/MethodController.java
@@ -1,13 +1,17 @@
 package com.itheima.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/method")
 public class MethodController {
-    @RequestMapping(method = RequestMethod.GET)
+
+    // -------------- The 1⃣️ way to use RequestMethod --------------
+    /*@RequestMapping(method = RequestMethod.GET)
     public void get() {
         System.out.println("RequestMethod.GET");
     }
@@ -25,5 +29,26 @@ public class MethodController {
     @RequestMapping(method = RequestMethod.PUT)
     public void put() {
         System.out.println("RequestMethod.PUT");
+    }*/
+
+    // -------------- The 2⃣️ way to use RequestMethod --------------
+    @GetMapping
+    public ModelAndView get() {
+        return new ModelAndView("success");
+    }
+
+    @GetMapping
+    public ModelAndView post() {
+        return new ModelAndView("success");
+    }
+
+    @GetMapping
+    public ModelAndView delete() {
+        return new ModelAndView("success");
+    }
+
+    @GetMapping
+    public ModelAndView put() {
+        return new ModelAndView("success");
     }
 }
