a5f7bbfcbeb43b9939c3eb71acb4943dc35e7247
TwilightLogic <twilightlogic@gmail.com>
2023-04-23 15:10:52 +0800

    Learn to request relavent parameters

--- SpringMvcTest/src/main/java/com/example/controller/MainController.java
+++ SpringMvcTest/src/main/java/com/example/controller/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,10 +18,10 @@ public class MainController {
 
     // 如果我们不希望有人携带`username`这个参数进入我们的网页，我们可以 ⬇️
     // 如果我们不希望`username`为`xxx`的人进入我们的网页，我们可以 下
-    @RequestMapping(value = "/index", params = "username!=xxx")
-    public ModelAndView index(){
-        return new ModelAndView("index");
-    }
+    //    @RequestMapping(value = "/index", params = "username!=xxx")
+    //    public ModelAndView index(){
+    //        return new ModelAndView("index");
+    //    }
 
     // 也可以直接写成 @PostMapping("/index") @GetMapping("/index")
     // 如果我们只希望通过GET, POST请求去访问这个路径，怎么写呢？（默认是GET请求）
@@ -44,5 +45,11 @@ public class MainController {
     //    public String index2() {
     //        return "index";
     //    }
-
+    // 注解获取请求相关参数
+    // `required=false`表示这个param参数不是一定要写的，不写的话也能访问的
+    @RequestMapping("/index")
+    public String index(@RequestParam(value = "username", required = false) String username) {
+        System.out.println("收到一个请求参数：" + username);
+        return "index";
+    }
 }
