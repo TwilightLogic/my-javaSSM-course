039bb9d8d84cab2341702d4b6108e85b66c86f28
TwilightLogic <twilightlogic@gmail.com>
2023-05-12 12:52:08 +0800

    Completed the all the  missions of project-in-class-01

--- project-in-class-01/src/main/java/com/gk/test/MybatisTest.java
+++ project-in-class-01/src/main/java/com/gk/test/MybatisTest.java
@@ -8,8 +8,27 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
+
+    @Test
+    public void findCustomerByNameTest() throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory =
+                new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        // SqlSession执行映射文件中定义的SQL，并返回映射结果
+        List<User> users = sqlSession.selectList("com.gk.mapper"
+                + ".CustomerMapper.findCustomerByName", "l");
+        for (User user : users) {
+            //打印输出结果集
+            System.out.println(user);
+        }
+    }
+
     @Test
     public void findCustomerByIdTest() throws Exception {
         String resource = "mybatis-config.xml";
@@ -24,4 +43,5 @@ public class MybatisTest {
 
         sqlSession.close();
     }
+
 }
