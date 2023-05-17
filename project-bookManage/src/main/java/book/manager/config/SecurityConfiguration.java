ec914f0cfdebcf1c6eec4990a791effdf378e854
TwilightLogic <twilightlogic@gmail.com>
2023-05-17 17:16:37 +0800

    Using "remember me" function in spring security of ssm

--- project-bookManage/src/main/java/book/manager/config/SecurityConfiguration.java
+++ project-bookManage/src/main/java/book/manager/config/SecurityConfiguration.java
@@ -1,14 +1,20 @@
 package book.manager.config;
 
 import book.manager.service.UserAuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -17,13 +23,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Resource
     UserAuthService service;
 
+    @Resource
+    PersistentTokenRepository repository;
+
+    // 利用数据库的持久化存储机制，即使服务器重新启动，所有的Token信息（remember me）也不会丢失⬇️
+    @Bean
+    public PersistentTokenRepository jdbcRepository(@Autowired DataSource dataSource){
+        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();  // 使用基于JDBC的实现
+        repository.setDataSource(dataSource);   // 配置数据源
+        // repository.setCreateTableOnStartup(true);   // 启动时自动创建用于存储Token的表（建议第一次启动之后删除该行）
+        return repository;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()   // 配置哪些请求会被拦截，哪些请求必须具有什么角色才能访问
                 .antMatchers("/static/**").permitAll()    // 静态资源，使用permitAll来运行任何人访问
-                .antMatchers("/**").hasAnyRole("user", "admin")     // 所有请求必须登录并且是user角色才可以访问（不包含上面的静态资源）
-                .anyRequest().hasRole("admin")
+                .antMatchers("/**").hasRole("user")     // 所有请求必须登录并且是user角色才可以访问（不包含上面的静态资源）
                 .and()
                 .formLogin()
                 .loginPage("/login")
@@ -35,7 +52,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/login")
                 .and()
-                .csrf().disable();
+                .csrf().disable()
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(60 * 60 * 24 * 7)
+                .tokenRepository(repository);
     }
 
     // 用来处理用户的登录
